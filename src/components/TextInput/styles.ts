d64c2d362a836584e048d5c5fddf3b40021871e3
Marcos Dourado <marcos@beestart.com.br>
2021-02-03 09:32:59 -0300

    finalizando o projeto

--- src/components/TextInput/styles.ts
+++ src/components/TextInput/styles.ts
@@ -4,7 +4,7 @@ import { IRON, NEVADA } from "../../utils/colors";
 
 const styles = StyleSheet.create({
   inputContainer: {
-    marginBottom: scale(16),
+    marginBottom: scale(16)
   },
   inputWrapper: {
     borderWidth: 1,
@@ -12,41 +12,41 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     height: scale(56),
     paddingTop: scale(0),
-    paddingHorizontal: scale(16),
+    paddingHorizontal: scale(16)
   },
   labeWrapper: {
     position: "absolute",
     left: 0,
-    width: "100%",
+    width: "100%"
   },
   label: {
     color: NEVADA,
     fontSize: 12,
-    fontFamily: "OverpassRegular",
+    fontFamily: "OverpassRegular"
   },
   textInputWrapper: {
     flex: 1,
     flexDirection: "row",
-    alignItems: "center",
+    alignItems: "center"
   },
   input: {
     fontFamily: "OverpassRegular",
     fontSize: 16,
-    height: "100%",
+    height: "100%"
   },
   inputLabed: {
     fontFamily: "OverpassRegular",
-    fontSize: 16,
+    fontSize: 16
   },
   iconRight: {
-    marginRight: scale(5),
+    marginRight: scale(5)
   },
   iconLeft: {
-    marginLeft: scale(5),
+    marginLeft: scale(5)
   },
   ajustIconAlign: {
-    height: "100%",
-  },
+    height: "100%"
+  }
 });
 
 export default styles;
